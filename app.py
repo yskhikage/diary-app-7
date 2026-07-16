56366f2f225c18e40e172ce5ac63bf084158bb73
yskhikage` <176762409+yskhikage@users.noreply.github.com>
2026-07-16 14:50:31 +0900

    編集処理を追加、Formのラベルを一部変更

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ from flask_wtf import FlaskForm
 
 
 
-
-
 app = Flask(__name__)
 #設定読み込み
 app.config.from_object('config')
@@ -71,11 +69,11 @@ def register():
         event = Event(name=name,date=date,created_at=datetime.now())
         db.session.add(event)
         db.session.commit()
-        #カレンダーに
+        #カレンダーにリダイレクト
         return redirect(url_for("calender"))
     #フォームにはデータが入力されていない状態だと、if文の中は処理されず、登録ページを表示する
     print('formを受け取れてないっすね')
-    return render_template("register.html", form=form)
+    return render_template("register.html", form=form,form_title='追加画面',event="")
 
 @app.route('/calendar/<int:id>/delete',methods=['POST'])
 def delete(id):
@@ -83,6 +81,22 @@ def delete(id):
     db.session.delete(event)
     db.session.commit()
     return redirect(url_for('calender'))
+
+
+@app.route('/calendar/<int:id>/edit',methods=['GET','POST'])
+def edit_event(id):
+    event=Event.query.get(id)
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        date = form.date.data
+        event = Event(name=name,date=date,updated_at=datetime.now())
+        db.session.merge(form)
+        db,session.commit()
+        return redirect(url_for("calender"))
+    print('編集画面では項目は受け取れていません')
+    return render_template("register.html", form=form,form_title='編集画面',event=event)
+
 
 
 if __name__ == '__main__':
--- form.py
+++ form.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, redirect, session
 from flask_wtf import FlaskForm
-from wtforms import ValidationError, StringField,DateField,SubmitField
+from wtforms import ValidationError, StringField,DateField,SubmitField,HiddenField
 from wtforms.validators import DataRequired
 
 class RegistrationForm(FlaskForm):
-    name = StringField('ユーザー名', validators=[DataRequired()])
-    date = DateField('日付を記入してください:', format='%Y-%m-%d')
+    id =HiddenField()
+    name = StringField('イベント名', validators=[DataRequired()])
+    date = DateField('日付:', format='%Y-%m-%d')
     submit = SubmitField()
